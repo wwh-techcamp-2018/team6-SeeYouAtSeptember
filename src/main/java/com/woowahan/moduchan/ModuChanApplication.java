29ea04ab3223c9afaa07402171aba20dfbc92f84
우아테크캠프_18 <woowatechcamp_18@woowatechcamp18.local>
2018-08-19 17:17:04 +0900

    spring cloud aws 의존성 추가 및 S3Uploader 클래스 생성
    1. aws.yml을 생성하였습니다.
            - aws.yml은 local에서 사용할 수 있도록 accessKey와 secretKey를 가지고 있습니다.
              .gitignore에 aws.yml을 추가하였습니다.
    2. application.properties에 bucket명과 region을 추가하였습니다.
    3. S3Uploader 클래스를 생성하였습니다.
            - upload 메소드를 통해 MultipartFile을 File로 변환한 후 s3에 저장하고 해당 url을 반환합니다.
    4. ModuchanApplication.java에서 생성한 aws.yml을 사용할 수 있도록 수정하였습니다.

--- src/main/java/com/woowahan/moduchan/ModuChanApplication.java
+++ src/main/java/com/woowahan/moduchan/ModuChanApplication.java
@@ -2,6 +2,7 @@ package com.woowahan.moduchan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -10,7 +11,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableCaching
 public class ModuChanApplication {
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.properties,"
+            + "classpath:aws.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(ModuChanApplication.class, args);
+        new SpringApplicationBuilder(ModuChanApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
 }
