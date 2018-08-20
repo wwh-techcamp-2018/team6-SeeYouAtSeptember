7d16023e093facba7351b27db2adcd7ee8b9cfff
우아테크캠프_18 <woowatechcamp_18@woowatechcamp18.local>
2018-08-20 12:52:34 +0900

    swagger 설명 추가
    1. category, project의 controller에 각 메소드별 설명을 추가하였습니다.
    2. category, product의 dto에서 변수의 예제를 추가하였습니다.

--- src/main/java/com/woowahan/moduchan/controller/ApiCategoryController.java
+++ src/main/java/com/woowahan/moduchan/controller/ApiCategoryController.java
@@ -4,6 +4,9 @@ import com.woowahan.moduchan.domain.category.Category;
 import com.woowahan.moduchan.dto.category.CategoryDTO;
 import com.woowahan.moduchan.dto.project.ProjectGatherDTO;
 import com.woowahan.moduchan.service.CategoryService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,16 +24,31 @@ public class ApiCategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    @ApiOperation(value = "카테고리 전체 조회", notes = "모든 카테고리의 정보를 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "조회 성공")
+            //error에 대한 설명 추가
+    })
     @GetMapping("")
     public ResponseEntity<List<CategoryDTO>> list() {
         return new ResponseEntity<>(categoryService.getCategories(), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "카테고리 조회", notes = "특정 카테고리의 정보를 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "조회 성공")
+            //error에 대한 설명 추가
+    })
     @GetMapping("/{cid}")
     public ResponseEntity<Category> getCategory(@PathVariable("cid") Long cid) {
         return new ResponseEntity<>(categoryService.geCategory(cid), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "카테고리별 특정 페이지의 프로젝트 조회", notes = "특정 페이지의 프로젝트 정보를 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "조회 성공")
+            //error에 대한 설명 추가
+    })
     @GetMapping("/{cid}/page/{pageNo}")
     public ResponseEntity<List<ProjectGatherDTO>> getCategoryPage(@PathVariable("cid") Long cid, @PathVariable("pageNo") int pageNo) {
         return new ResponseEntity<>(categoryService.getCategoryPage(cid, pageNo), HttpStatus.OK);
--- src/main/java/com/woowahan/moduchan/dto/category/CategoryDTO.java
+++ src/main/java/com/woowahan/moduchan/dto/category/CategoryDTO.java
@@ -1,5 +1,6 @@
 package com.woowahan.moduchan.dto.category;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,7 +9,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CategoryDTO {
+    @ApiModelProperty(position = 1)
     private Long id;
+    @ApiModelProperty(example = "title", position = 2)
     private String title;
+    @ApiModelProperty(example = "https://i.ebayimg.com/images/g/LcsAAOSwc6Jaem96/s-l300.jpg", position = 3)
     private String categoryImageUrl;
 }
