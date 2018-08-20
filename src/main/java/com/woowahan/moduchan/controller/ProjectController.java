7be4cb39528d137d50a0d6c0c564dddf019992af
우아테크캠프_25 <woowatechcamp_25@woowatechcamp25.local>
2018-08-20 20:20:07 +0900

    feat: project detail page를 생성하였습니다.
    1. 프로젝트의 상세보기를 위한 템플릿페이지 생성
    
    todo
    - 실제 프로젝트 내용을 모델로 넘겨 내용 채우기
    - 마크다운 뷰어를 이용해 설명구역 바꾸기

--- src/main/java/com/woowahan/moduchan/controller/ProjectController.java
+++ src/main/java/com/woowahan/moduchan/controller/ProjectController.java
@@ -1,9 +1,15 @@
 package com.woowahan.moduchan.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/project")
 public class ProjectController {
+    @GetMapping("/{pid}")
+    public String getProject(@PathVariable("pid") Long pid){
+        return "project-detail";
+    }
 }
