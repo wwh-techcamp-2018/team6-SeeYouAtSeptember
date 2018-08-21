2b85295add100d75c9bb53f15760a492e3467abc
우아테크캠프_18 <woowatechcamp_18@woowatechcamp18.local>
2018-08-21 19:06:17 +0900

    properties error 수정
    1. aws.properties를 local에서 사용하기 위해 AmazonConfig 클래스를 생성하였습니다.
    2. lombok dependency를 최신으로 수정하였습니다.

--- src/main/java/com/woowahan/moduchan/ModuChanApplication.java
+++ src/main/java/com/woowahan/moduchan/ModuChanApplication.java
@@ -1,22 +1,14 @@
 package com.woowahan.moduchan;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableCaching
 public class ModuChanApplication {
 
-    public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "classpath:application.properties,"
-            + "classpath:aws.yml";
-
     public static void main(String[] args) {
-        new SpringApplicationBuilder(ModuChanApplication.class)
-                .properties(APPLICATION_LOCATIONS)
-                .run(args);
+        SpringApplication.run(ModuChanApplication.class, args);
     }
 }
