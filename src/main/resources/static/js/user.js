4f1a25f368e982d1c49681c5fe08b66fcc70ca8d
우아테크캠프_6 <woowatechcamp_6@woowatechcamp6.local>
2018-08-26 02:55:07 +0900

    주소 api 사용
    1. daum 주소 api를 통해 주소 검색 기능 구현
    	- 주소가 검색한 주소와 다를 시 유효성 검사

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -35,7 +35,7 @@ class Join {
     constructor() {
         addEventListenerToTarget($("#email-domain-select"), "change", this.domainSelectChangeHandler.bind(this))
         addEventListenerToTarget($(".join-form-box .btn"), "click", this.joinBtnClickHandler.bind(this));
-        this.focusOutTargetList = [$("input#email-domain"), $("input#pw1"), $("input#pw2"), $("input#name"), $("input#cell3"), $("input#address")];
+        this.focusOutTargetList = [$("input#email-domain"), $("input#pw1"), $("input#pw2"), $("input#name"), $("input#cell3")];
         this.focusOutTargetList.forEach(target => {
             addEventListenerToTarget(target, "focusout", this.focusOutHandler.bind(this));
         })
@@ -52,8 +52,6 @@ class Join {
             this.validName();
         if (evt.target.id === "cell3")
             this.validPhoneNo();
-        if (evt.target.id === "address")
-            this.validAddress();
     }
 
     domainSelectChangeHandler(evt) {
@@ -162,11 +160,13 @@ class Join {
     }
 
     validAddress() {
-        this.address = $("#address").value;
-        if (!this.address) {
+        this.address = $("#postcode").value + $("#address").value;
+        
+        if (this.address !== addressAPI.address) {
             $("#address-caution").style.display = "inline-block";
             return false;
         }
+        this.address += $("#address_detail").value;
         $("#address-caution").style.display = "none";
         return true;
     }
@@ -180,7 +180,68 @@ class Join {
 
 }
 
+class AddressAPI{
+        constructor(){
+            addEventListenerToTarget($("#address-search-btn"), "click", this.loadDaum.bind(this));
+            addEventListenerToTarget($("#btnCloseLayer"),"click", this.closeDaumPostcode.bind(this));
+        }
+
+    loadDaum (evt) {
+        evt.preventDefault();
+        const elementLayer = $('#layer');
+        const currentThis = this;
+        daum.postcode.load(function(){
+          new daum.Postcode({
+            oncomplete: function(data) {
+              let fullAddr = '';
+              let extraAddr = '';
+              if (data.userSelectedType === 'R') {
+                fullAddr = data.roadAddress;
+              } else {
+                fullAddr = data.jibunAddress;
+              }
+
+              if(data.userSelectedType === 'R'){
+                if(data.bname !== ''){
+                  extraAddr += data.bname;
+                }
+                if(data.buildingName !== ''){
+                  extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+                }
+                fullAddr += (extraAddr !== '' ? ' ('+ extraAddr +')' : '');
+              }
+
+              currentThis.address= data.zonecode + fullAddr;
+              $("#postcode").value = data.zonecode;
+              $("#address").value =  fullAddr;
+        
+              elementLayer.style.display = 'none';
+            }
+          }).embed(elementLayer,currentThis);
+        })
+        elementLayer.style.display = 'block';
+        
+        this.initLayerPosition(elementLayer);
+      }
+
+      initLayerPosition(elementLayer){
+        const width = 500;
+        const height = 400;
+        const borderWidth = 2;
+        elementLayer.style.width = width + 'px';
+        elementLayer.style.height = height + 'px';
+        elementLayer.style.border = borderWidth + 'px solid';
+        elementLayer.style.left = (((window.innerWidth || document.documentElement.clientWidth) - width)/2 - borderWidth) + 'px';
+        elementLayer.style.top = (((window.innerHeight || document.documentElement.clientHeight) - height)/2 - borderWidth) + 'px';
+      }
+
+      closeDaumPostcode(){
+        $("#layer").style.display='none';
+      }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     new LoginBtn();
     new Join();
+    addressAPI = new AddressAPI();
 });
