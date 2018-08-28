dfadcc86ce596bd8580054b675dda93f96880ca8
mindock <woowatechcamp_18@woowatechcamp18.local>
2018-08-28 18:10:10 +0900

    feat: donate cart 구현
    1. 상품의 갯수를 선택하고 담기버튼을 누르면 상단에 상품 이름, 갯수가 나타난다.
    2. 같은 상품을 담으면, 카트의 갯수가 더해진다.
    3. 남아있는 갯수보다 많은 갯수를  후원하려면 input tag가 빨갛게 변합니다.
    4. 화살표로 갯수를 조정하는 경우, min과 max값으로 범위를 지정했습니다.

--- src/main/resources/static/js/project_detail.js
+++ src/main/resources/static/js/project_detail.js
@@ -1,6 +1,6 @@
 class ProductBtns {
     constructor() {
-        this.SUPPORT_BTN_TEXT = "후원하기"
+        this.SUPPORT_BTN_TEXT = "담기"
         this.CANCEL_BTN_TEXT = "취소"
 
         this.productBtns = $all(".product-list .product-btn");
@@ -9,15 +9,28 @@ class ProductBtns {
             addEventListenerToTarget(btn, "click", this.productBtnClickHandler.bind(this));
         });
         addEventListenerToTarget($(".need-login-cover"), "click", this.needLoginCoverClickHandler);
+        addEventListenerToTarget($(".support-btn"), "click", this.supportBtnClickHandler.bind(this));
+        addEventListenerToTarget($(".support-product-cart"),"click", this.xBtnHandler.bind(this));
     }
 
     needLoginCoverClickHandler() {
         window.location.href = "/users/login";
     }
 
+    xBtnHandler(evt) {
+        if(evt.target.className !== "close") {
+            return;
+        }
+        evt.target.parentElement.remove();
+    }
+
+    supportBtnClickHandler(evt) {
+        this.supportProduct($(".support-product-cart"));
+    }
+
     productBtnClickHandler(evt) {
         if (evt.target.textContent === this.SUPPORT_BTN_TEXT) {
-            this.supportProduct(evt.target);
+            this.addCart(evt.target);
             return;
         }
         if (evt.target.textContent === this.CANCEL_BTN_TEXT) {
@@ -29,9 +42,64 @@ class ProductBtns {
             target.querySelector(".product-info-container .product-support").style.display = "block";
     }
 
+    insertCartHTML(target) {
+        const id = target.closest("div.product-btn").dataset.productId;
+        const suppliedQuantity = target.parentElement.firstElementChild.max;
+        const productPrice = target.parentElement.parentElement.getElementsByClassName("product-info-price").item(0).lastElementChild.textContent.replace(/[^0-9]/g, '');
+        const quantity = target.parentElement.firstElementChild.value;
+        const name = target.parentElement.parentElement.getElementsByClassName("product-info-title").item(0).textContent;
+
+        let html = `<li data-product-price="${productPrice}" data-product-id="${id}" >
+                        <div class="title">${name}</div>
+                        <div class="amount">
+                            <input type="number" value="${quantity}" min="1" max="${suppliedQuantity}"/>개
+                        </div>
+                        <div class="close">&times;</div>
+                    </li>`;
+        $(".support-product-cart ul").insertAdjacentHTML("beforeend", html);
+    }
+
+    appendCartHTML(matchCart, target) {
+        let amount = $at(matchCart,".amount input");
+        const inputQuantity = target.parentElement.firstElementChild.value;
+        amount.value = parseInt(amount.value) + parseInt(inputQuantity);
+    }
+
+    showOverAmount(target, milleSec) {
+        const cautionElement = target.parentElement.querySelector(".caution.over");
+        cautionElement.style.display = "block";
+        setTimeout(function () {
+            cautionElement.style.display = "none";
+        }, milleSec)
+    }
+
+    addCart(target) {
+        if (!this.validQuantity(target)) {
+            return;
+        }
+
+        const quantity = target.parentElement.firstElementChild.value;
+        const suppliedQuantity = target.parentElement.firstElementChild.max;
+        if(parseInt(quantity) > parseInt(suppliedQuantity)) {
+            this.showOverAmount(target, 2000);
+            return;
+        }
+
+        const id = target.closest("div.product-btn").dataset.productId;
+        const cartList = [...$(".support-product-cart ul").children];
+        const matchCart = cartList.filter(cart => {return cart.dataset.productId === id});
+
+        if(matchCart.length === 0) {
+            this.insertCartHTML(target);
+        } else {
+            this.appendCartHTML(matchCart[0], target);
+        }
+        this.hideProductSupport(target);
+    }
+
     hideProductSupport(target) {
-        target.parentElement.firstElementChild.value = "";
-        target.closest("div.product-support").style.display = "none";
+        $at(target.parentElement, ".product-quantity").value="";
+        target.parentElement.style.display = "none";
     }
 
     validQuantity(target) {
@@ -51,29 +119,56 @@ class ProductBtns {
         }, milleSec)
     }
 
-    supportProduct(target) {
-        if (!this.validQuantity(target)) {
-            return;
-        }
-        const productPrice = target.parentElement.parentElement.getElementsByClassName("product-info-price").item(0).lastElementChild.textContent.replace(/[^0-9]/g, '');
-        const quantity = target.parentElement.firstElementChild.value;
-        const name = target.parentElement.parentElement.getElementsByClassName("product-info-title").item(0).textContent;
+    setSupportForm(liElement) {
+        const quantity = $at(liElement, ".amount input").innerText;
+        const productPrice = liElement.dataset.productPrice;
 
         const supportForm = {
-            "pid": target.closest("div.product-btn").dataset.productId,
+            "pid": liElement.dataset.productId,
             "quantity": quantity,
             "purchasePrice": productPrice * quantity,
-            "name": name
+            "name": $at(liElement, ".title").innerText
         };
+        return supportForm;
+    }
+
+    showCartAmount(filterCart) {
+        filterCart.forEach(cartLi => {
+            $at(cartLi,".amount input").style.borderColor = "red";
+            setTimeout(function () {
+                $at(cartLi,".amount input").style.borderColor = "";
+            }, 2000)});
+    }
+
+    validCartAmount(target) {
+        const inputTag = $at(target,".amount input");
+        if(parseInt(inputTag.value) > parseInt(inputTag.max)) {
+            return false;
+        }
+        return true;
+    }
+
+    supportProduct(target) {
+        const cartList =  [...$(".support-product-cart").firstElementChild.children];
+        const supportFormList = [];
+        const filterCart = cartList.filter(cart => {return !this.validCartAmount(cart)});
+
+        if(filterCart.length !== 0) {
+            this.showCartAmount(filterCart);
+            return;
+        }
+
+        for(const cart of cartList) {
+            supportFormList.push(this.setSupportForm(cart));
+        }
 
         fetchManager({
             url: '/api/orders',
             method: 'POST',
             headers: {'content-type': 'application/json'},
-            body: JSON.stringify(supportForm),
+            body: JSON.stringify(supportFormList),
             callback: this.supportCallback.bind(this)
         });
-        this.hideProductSupport(target);
     }
 
     supportCallback(response) {
