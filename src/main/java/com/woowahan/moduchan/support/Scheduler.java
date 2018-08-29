e72ac91b787718c9cc5efe128a58e1a3ad94534b
choiyusung <chldbtjd7530@naver.com>
2018-08-29 22:23:42 +0900

    Feat/payment (#115)
    * pending order 스케줄러 추가
    
    1. 결제 대기중 상태일 경우 5분이 지나면 실패로 업데이트한다.
    	- 스케줄러가 5분마다 확인한다.
    
    * 주문 실패시 orderHistory table update
    
    1. 주문하기가 실패시 orderHistory가 fail로 적용된다.
    2. product 수량을 원상복귀 시켜준다.
    
    * 포맷팅 작업
    
    * 스웨거 정리
    
    1. orderController의 api 스웨서 상세 설명 정리

---
+++ src/main/java/com/woowahan/moduchan/support/Scheduler.java
@@ -0,0 +1,18 @@
+package com.woowahan.moduchan.support;
+
+import com.woowahan.moduchan.service.OrderHistoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Scheduler {
+
+    @Autowired
+    private OrderHistoryService orderHistoryService;
+
+    @Scheduled(cron = "0 0/5 * * * *")
+    public void updatePendingOrderHistory() {
+        orderHistoryService.updatePendingOrderHistory();
+    }
+}
